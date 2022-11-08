c1b57fe200da724eaf58388dd94108e5379bc6c7
Tonyginto <antik998@mail.ru>
2022-11-08 18:12:34 +0300

    extra comment removed

--- Tbot.py
+++ Tbot.py
@@ -10,7 +10,7 @@ import pandas as pd
 # импортируем классы Players из файла AlSQL.py
 from AlSQL import Players
 
-engine = create_engine('sqlite:///players.db')
+engine = create_engine('sqlite:///players.db', connect_args={'check_same_thread': False})
 # Свяжем engine с метаданными класса Players,
 # чтобы декларативы могли получить доступ через экземпляр DBSession
 Players.metadata.bind = engine
@@ -30,40 +30,15 @@ def get_text_messages(message):
     key_baw = types.KeyboardButton(text='Указать/Изменить желание')
     key_show = types.KeyboardButton(text='Показать моё имя и желание')
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True).row(key_bns,key_baw).row(key_show)
-    # message.reply("Привет", reply_markup=keyboard)
-    # keyboard.add(key_bns, key_baw).add(key_show)
-    # bot.edit_message_reply_markup(message.chat.id, message.message_id, 'Выбери:', reply_markup=keyboard)
-    # bot.send_message(message.from_user.id, "Ваш текст", reply_markup=keyboard)
-
-    # kb = [
-    #     [
-    #         types.KeyboardButton(text="Указать/Изменить имя и фамилию"),
-    #         types.KeyboardButton(text="Указать/Изменить желание"),
-    #         types.KeyboardButton(text="Показать моё имя и желание")
-    #     ],
-    # ]
-    # keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
-    # message.reply(reply_markup=keyboard)
 
     if message.text == 'Указать/Изменить имя и фамилию':
         bot.send_message(message.from_user.id, "Введи свои имя и фамилию")
         bot.register_next_step_handler(message, get_NameAndSurname)
-        # get_NameAndSurname(message)
     elif message.text == 'Указать/Изменить желание':
         bot.send_message(message.from_user.id, "Введи своё желание")
         bot.register_next_step_handler(message, get_wish)
-        # get_wish(message)
     elif message.text == 'Показать моё имя и желание':
-        # bot.register_next_step_handler(message, show_wish_name)
         show_wish_name(message)
-
-    # if message.text == "/start" or message.text == "/reg":
-    #     bot.send_message(message.from_user.id, "Привет, введи свои имя и фамилию")
-    #     bot.register_next_step_handler(message, get_NameAndSurname)
-    # elif message.text == "/help":
-    #     bot.send_message(message.from_user.id, "Введи имя и фамилию, после чего укажи пожелания для подарка")
-    # else:
-    #     bot.send_message(message.from_user.id, "Излишняя информация")
 
 
 def get_NameAndSurname(message):
@@ -78,16 +53,6 @@ def get_NameAndSurname(message):
         session.commit()
     bot.send_message(message.from_user.id, "Изменено")
 
-    # ns = message.text
-    # keyboard = types.InlineKeyboardMarkup();  # наша клавиатура
-    # key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes');  # кнопка «Да»
-    # keyboard.add(key_yes);  # добавляем кнопку в клавиатуру
-    # key_no = types.InlineKeyboardButton(text='Нет', callback_data='no');
-    # keyboard.add(key_no);
-    # question = 'Тебя зовут ' + str(ns) + '?';
-    # bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
-    # ns = bot.register_next_step_handler(message, get_surnme)
-
 def get_wish(message):
     player = session.query(Players).filter_by(id_telegram=message.from_user.id).one()
     player.wish = message.text
@@ -99,15 +64,6 @@ def show_wish_name(message):
     player = session.query(Players).filter_by(id_telegram=message.from_user.id).one()
     out_txt = "Имя: "+str(player.name_surname) + "\nЖелание: " + str(player.wish)
     bot.send_message(message.from_user.id, out_txt)
-
-
-# @bot.callback_query_handler(func=lambda call: True)
-# def callback_worker(call):
-#     if call.data == "yes":  # call.data это callback_data, которую мы указали при объявлении кнопки
-#         bot.send_message(call.message.chat.id, 'Запомню : )');
-#     elif call.data == "no":
-#         bot.send_message(call.message.from_user.id, "Привет, введи свои имя и фамилию")
-#         bot.register_next_step_handler(call.message, get_NameAndSurname)
 
 
 bot.polling(none_stop=True, interval=0)

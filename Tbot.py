d875bc22858da0cac19368b8d478dd7204f0311a
Tonyginto <antik998@mail.ru>
2022-11-11 12:34:21 +0300

    added new buttons in Tbot and changes random in TestAlchemy

--- Tbot.py
+++ Tbot.py
@@ -1,13 +1,12 @@
-import os
+import datetime
 import telebot
-# from keyboa import keyboa_maker
 from telebot import types
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import table, column, select
 import pandas as pd
-# импортируем классы Players из файла AlSQL.py
+# импортируем класс Players из файла AlSQL.py
 from AlSQL import Players
 
 engine = create_engine('sqlite:///players.db', connect_args={'check_same_thread': False})
@@ -29,27 +28,46 @@ def get_text_messages(message):
     key_bns = types.KeyboardButton(text='Указать/Изменить имя и фамилию')
     key_baw = types.KeyboardButton(text='Указать/Изменить желание')
     key_show = types.KeyboardButton(text='Показать моё имя и желание')
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True).row(key_bns,key_baw).row(key_show)
+    key_showrec = types.KeyboardButton(text='Показать имя и желание одаряемого')
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True).row(key_bns,key_baw).row(key_show, key_showrec)
+    # bot.send_message(message.from_user.id, "Ваш текст", reply_markup=keyboard)
 
-    if message.text == 'Указать/Изменить имя и фамилию':
+    if message.text == 'Указать/Изменить имя и фамилию' or message.text == '/insname':
         bot.send_message(message.from_user.id, "Введи свои имя и фамилию")
         bot.register_next_step_handler(message, get_NameAndSurname)
-    elif message.text == 'Указать/Изменить желание':
+    elif message.text == 'Указать/Изменить желание' or message.text == '/inswish':
         bot.send_message(message.from_user.id, "Введи своё желание")
         bot.register_next_step_handler(message, get_wish)
-    elif message.text == 'Показать моё имя и желание':
+    elif message.text == 'Показать моё имя и желание' or message.text == '/showmywish':
         show_wish_name(message)
-
+    elif message.text == 'Показать имя и желание одаряемого' or message.text == '/showrecwish':
+        show_gift_recepient(message)
+    elif message.text == '/help':
+        bot.send_message(message.from_user.id, "Введи 'Указать/Изменить имя и фамилию'(/insname) или "
+                                               "'Указать/Изменить желание'(/inswish) или "
+                                               "'Показать моё имя и желание'(/showmywish) или "
+                                               "'Показать имя и желание одаряемого'(/showmywish )")
+    else:
+        bot.send_message(message.from_user.id, "Введи /help")
 
 def get_NameAndSurname(message):
-    player = session.query(Players).filter_by(id_telegram=message.from_user.id).one()
-    if player == []:
-        playerone = Players(id_telegram=message.from_user.id, name_surname=message.text, wish="", is_choose=False)
-        session.add(playerone)
-        session.commit()
-    else:
+    # player = session.query(Players).filter_by(id_telegram=message.from_user.id).one()
+    # if player == []:
+    #     playerone = Players(id_telegram=message.from_user.id, name_surname=message.text, wish="", is_choose=False)
+    #     session.add(playerone)
+    #     session.commit()
+    # else:
+    #     player.name_surname = message.text
+    #     session.add(player)
+    #     session.commit()
+    try:
+        player = session.query(Players).filter_by(id_telegram=message.from_user.id).one()
         player.name_surname = message.text
         session.add(player)
+        session.commit()
+    except:
+        playerone = Players(id_telegram=message.from_user.id, name_surname=message.text, wish="", is_choose=False)
+        session.add(playerone)
         session.commit()
     bot.send_message(message.from_user.id, "Изменено")
 
@@ -62,8 +80,20 @@ def get_wish(message):
 
 def show_wish_name(message):
     player = session.query(Players).filter_by(id_telegram=message.from_user.id).one()
-    out_txt = "Имя: "+str(player.name_surname) + "\nЖелание: " + str(player.wish)
+    out_txt = "Имя: "+str(player.name_surname) + "\nЖелание: " + str(player.wish) if player != [] else "Сначала укажите своё имя и желание"
     bot.send_message(message.from_user.id, out_txt)
 
+def show_gift_recepient(message):
+    id_rec = session.query(Players).filter_by(id_telegram=message.from_user.id).one().choose_id
+    if id_rec != None:
+        recepient = session.query(Players).filter_by(id_telegram=id_rec).one()
+        out_txt = "Задача оформить подарок для "+str(recepient.name_surname) + "\nЕго желание: " + str(recepient.wish)
+    else:
+        out_txt = "Одаряемый ещё не объявился"
+    bot.send_message(message.from_user.id, out_txt)
 
-bot.polling(none_stop=True, interval=0)
+while True:
+    try:
+        bot.polling(none_stop=True, interval=0)
+    except:
+        print(datetime.datetime.now())
--- TestAlchemy.py
+++ TestAlchemy.py
@@ -1,3 +1,6 @@
+import random
+import keyboa
+from keyboa import keyboards
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import table, column, select
@@ -17,13 +20,9 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-# playerone = Players(id_telegram=1, name_surname="Антон Клягин", wish="мятная", is_choose=False)
+# playerone = Players(id_telegram=3, name_surname="asdsdasddd", wish="мятная", is_choose=False)
 # session.add(playerone)
 # session.commit()
-
-player = session.query(Players).filter_by(id_telegram=351637638).one()
-out_txt = "Имя: " + str(player.name_surname) + "\nЖелание: " + str(player.wish)
-print(out_txt)
 
 # player = session.query(Players).filter_by(id_telegram=1).one()
 # print(player.id)
@@ -31,12 +30,10 @@ print(out_txt)
 # session.add(player)
 # session.commit()
 
-# bookToDelete = session.query(Players).filter_by(id_telegram=1).first()
+# bookToDelete = session.query(Players).filter_by(id=2).first()
 # session.delete(bookToDelete)
 # session.commit()
 
-df = pd.read_sql_table('players', engine)
-print(df)
 # print(session.query(Players).sellect(id=1).all())
 
 
@@ -50,3 +47,34 @@ print(df)
 
 # print(session.execute(select(t)).first()[0])
 # print(s)
+
+def randoming_players():
+    all_play = session.query(Players).filter_by(is_choose=False).all()
+    num = len(all_play)
+    l, f = list(range(0, num)), False
+    while not f:
+        random.shuffle(l)
+        f = [False if i == l[i] else True for i in range(num)]
+        f = True if all(f) else False
+        # i: int = 0
+        # while i < num:
+        #     if i == l[i]:
+        #         random.shuffle(l)
+        #         break
+        #     i += 1
+        # if i == num: f = True
+    for i in range(num):
+        all_play[i].choose_id, all_play[i].is_choose = all_play[l[i]].id_telegram , True
+        session.add(all_play[i])
+        session.commit()
+
+def allFalse():
+    for i in session.query(Players).filter_by(is_choose=True).all():
+        i.is_choose, i.choose_id = False, None
+        session.add(i)
+        session.commit()
+
+allFalse()
+randoming_players()
+df = pd.read_sql_table('players', engine)
+print(df)
